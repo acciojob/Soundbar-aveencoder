1cdf6a7fb8a04dd046fa83134555212285080383
aveencoder <aveen598@gmail.com>
2025-09-21 12:58:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 //your JS code here. If required.
-jdijfosjfojsofiasiojaso
-lsdfoajsdofj
+   let currentAudio = null;
+
+    function playSound(fileName) {
+      // stop currently playing
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+      currentAudio = new Audio('sounds/' + fileName);
+      currentAudio.play();
+    }
+
+    function stopSound() {
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+    }
+
+    document.getElementById('buttons').addEventListener('click', function(event) {
+      const target = event.target;
+      if (target.classList.contains('btn')) {
+        const soundFile = target.getAttribute('data-sound');
+        if (soundFile) {
+          playSound(soundFile);
+        }
+      }
+      else if (target.classList.contains('stop')) {
+        stopSound();
+      }
+    });
